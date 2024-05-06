ee65ff21addbb563442508d8218fbe4d43de645e
(Nam) Pham Tien <phamtiennam2301@gmail.com>
2024-05-06 14:36:14 +0700

    feat: change code

--- cmd/bot/worker.go
+++ cmd/bot/worker.go
@@ -2,14 +2,14 @@ package bot
 
 import (
 	"crypto/ecdsa"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/tiennampham23/berachain-airdrop/pkg/config"
-	"github.com/tiennampham23/berachain-airdrop/pkg/dapp/mintaura"
 	"github.com/tiennampham23/berachain-airdrop/pkg/log"
-	"github.com/tiennampham23/berachain-airdrop/pkg/utilties/eth"
-	"math/big"
+)
+
+const (
+	NativeToken = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
 )
 
 type Bot struct {
@@ -42,6 +42,7 @@ func (b *Bot) Start() error {
 
 	for _, privateKey := range privateKeys {
 		log.Logger().Infow("Private key", "privateKey", privateKey)
+		// Interact with dapps
 	}
 	return nil
 }
@@ -56,46 +57,4 @@ func (b *Bot) getPrivateKeys() ([]*ecdsa.PrivateKey, error) {
 		privateKeys = append(privateKeys, privateKey)
 	}
 	return privateKeys, nil
-}
-
-func (b *Bot) claimNFTMintAura(fromAddress common.Address, privateKey *ecdsa.PrivateKey) error {
-	quantity := big.NewInt(1)
-	currency := common.HexToAddress("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE")
-	pricePerToken := big.NewInt(50000000000000000)
-
-	quantityLimitPerWallet, _ := new(big.Int).SetString("115792089237316195423570985008687907853269984665640564039457584007913129639935", 10)
-	byte32 := [32]byte{}
-	allowlistProof := mintaura.IDropAllowlistProof{
-		Proof:                  [][32]byte{byte32},
-		QuantityLimitPerWallet: quantityLimitPerWallet,
-		PricePerToken:          pricePerToken,
-		Currency:               currency,
-	}
-
-	var data []byte
-
-	payload, err := mintaura.Claim(
-		fromAddress,
-		quantity,
-		currency,
-		pricePerToken,
-		allowlistProof,
-		data,
-	)
-
-	if err != nil {
-		log.Logger().Errorw("Failed to claim NFT Mint Aura", "err", err, "address", fromAddress)
-		return err
-	}
-
-	to := common.HexToAddress("0x6Dc544b917904959e5c26b9202907ca16BDcdb4A")
-
-	tx, err := eth.SendTx(b.ethClient, privateKey, payload, pricePerToken, to)
-	if err != nil {
-		log.Logger().Errorw("Failed to claim NFT Mint Aura", "err", err, "address", fromAddress)
-		return err
-	}
-
-	log.Logger().Infow("Mint NFT Aura successfully", "tx", tx.Hash().Hex())
-	return nil
 }
